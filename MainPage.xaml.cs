3b4505472f769547e26440f21b54e403fd948dbb
seasiainfotech <seasia.developer@gmail.com>
2019-03-20 17:09:39 +0530

    Commited by rakesh

---
+++ MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FormsPOC.ViewModel;
+using Xamarin.Forms;
+
+namespace FormsPOC
+{
+    /// <summary>
+    /// Main page.This class used to initialize components of Mainpage
+    /// </summary>
+    public partial class MainPage : ContentPage
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainPage class.
+        /// </summary>
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            InitializeComponent();
+            this.BindingContext = new MainPageViewModel();
+        }
+    }
+}
---
+++ MainPageViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using FormsPOC.Model;
+using Plugin.Connectivity;
+using Plugin.DeviceInfo;
+using Xamarin.Forms;
+
+namespace FormsPOC.ViewModel
+{
+    /// <summary>
+    /// Main page view model.This class use to set data to UI
+    /// </summary>
+    public class MainPageViewModel:INotifyPropertyChanged
+    {
+        public ICommand CheckCommand { get; set; }
+        #region Model class property
+        private DeviceInfoModel _deviceInfo;
+        public DeviceInfoModel DeviceInfo
+        {
+            get
+            {
+                return _deviceInfo;
+            }
+            set
+            {
+                _deviceInfo = value;
+                if (PropertyChanged != null)
+                    OnPropertyChanged(nameof(DeviceInfo));
+
+            }
+        }
+        #endregion
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:FormsPOC.ViewModel.MainPageViewModel"/> class.
+        /// </summary>
+        public MainPageViewModel() 
+        {
+
+            CheckCommand = new Command(InternetCheck);
+            //call method to set data to model 
+            SetInfo();
+        }
+        /// <summary>
+        /// Sets the info of device and app to model class
+        /// </summary>
+        public void SetInfo() 
+        {
+            _deviceInfo = new DeviceInfoModel()
+            {
+                ApplicationVersion= CrossDeviceInfo.Current.AppVersion,
+                DeviceModel = CrossDeviceInfo.Current.Model,
+                Platform = Convert.ToString(CrossDeviceInfo.Current.Platform),
+                Version = CrossDeviceInfo.Current.Version,
+                Manufacturer = CrossDeviceInfo.Current.Manufacturer,
+                AppPackage = DependencyService.Get<IPackageName>().PackageName
+        };
+        }
+        /// <summary>
+        /// Internets the check.
+        /// </summary>
+        private void InternetCheck()
+        {
+            if (CrossConnectivity.Current.IsConnected)
+                Application.Current.MainPage.DisplayAlert("Connectivity", "You are connected with internet", null, "OK");
+            else
+                Application.Current.MainPage.DisplayAlert("Connectivity", "You are not connected with internet", null, "OK");
+        }
+        /// <summary>
+        /// Occurs when property changed.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
